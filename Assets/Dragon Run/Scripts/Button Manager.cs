1f33c1ea3547d53738e9145a6ce4e4b5aa0e9bd5
Layci <nmmk1213@naver.com>
2024-11-01 21:49:47 +0900

    인게임 씬 UI작업 완료 및 버그 픽스

--- Assets/Dragon Run/Scripts/Button Manager.cs
+++ Assets/Dragon Run/Scripts/Button Manager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
     public GameObject helpPanel;
+    public GameObject pausePanel;
 
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 1.0f;
-        helpPanel.SetActive(false);
+
+        if (helpPanel != null)
+        {
+            helpPanel.SetActive(false);
+        }
+
+        if (pausePanel != null)
+        {
+            pausePanel.SetActive(false);
+        }
     }
 
     public void StartBtn()
@@ -37,5 +48,17 @@ public class ButtonManager : MonoBehaviour
     public void CancelHelp()
     {
         helpPanel.SetActive(false);
+    }
+
+    public void PauseBtn()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void StartingBtn()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1.0f;
     }
 }
